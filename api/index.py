9489bc71f2c5b90e707b5419e4f526790d0c3eac
kxz <63444697+kxzazz@users.noreply.github.com>
2024-04-15 04:47:45 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,7 +4,4 @@ app = FastAPI()
 
 @app.get("/api/python")
 def hello_world():
-    print("HIT!!")
-    d = {"message": "Hello World"}
-    print(d)
-    return d
+    return {"message": "Hello World"}
